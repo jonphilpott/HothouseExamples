8e421a4c4e3c4647c0fac666ac8cdad332bdcf24
Jon Philpott <jon.philpott@gmail.com>
2025-12-29 20:42:30 -0700

    update delay times for frippertronics, add tape hiss emulation and scale the LFO frequency based on the delay time.

--- src/Frippertronics/frippertronics.cpp
+++ src/Frippertronics/frippertronics.cpp
@@ -32,8 +32,10 @@ using daisysp::OnePole;
 using daisysp::Limiter;
 using daisysp::fonepole;
 using daisysp::Oscillator;
+using daisysp::WhiteNoise;
 
-#define MAX_DELAY static_cast<size_t>(48000 * 23.0f)
+#define MAX_DELAY static_cast<size_t>(48000 * 30.0f)
+
 #define N_DELAYS (2)
 
 Hothouse hw;
@@ -45,20 +47,30 @@ struct delay_s {
   float feedback;
   // store the last value of the delay line for the cross-feedback.
   float lastVal;
+  float samplerate;
+  WhiteNoise *noise;
 
   // feedback filter lines
   OnePole *fb_lpf, *fb_hpf;
+
+  // tape noize filter
+  OnePole *tape_hpf;
 
   // LFO for delay time modulation
   Oscillator *lfo;
 
   float Process(float in) {
-    del->SetDelay(currentDelay + (lfo->Process() * 48));
+    float delay_fraction = currentDelay / (samplerate * 23.0f);
+
+    del->SetDelay(currentDelay + (lfo->Process() * delay_fraction));
     float read = del->Read();
-    
+
+    // add some noise
+    read = read + tape_hpf->Process(noise->Process());
+
     // apply the filters
     read = fb_lpf->Process(fb_hpf->Process(read));
-    
+
     // write back with softclip
     del->Write(daisysp::SoftClip((feedback * read) + in));
     lastVal = read;
@@ -74,21 +86,29 @@ Led led_erase;
 bool RECORDING = false;
 bool ERASING   = false;
 
-float CROSS_FEEDBACK = 0.0f;
-float FEEDBACK       = 0.0f;
+float CROSS_FEEDBACK   = 0.0f;
+float FEEDBACK         = 0.0f;
 
 delay_s DELAYS[N_DELAYS];
 OnePole FB_LPFS[N_DELAYS];
 OnePole FB_HPFS[N_DELAYS];
+OnePole TAPE_HPFS[N_DELAYS];
 Oscillator LFOS[N_DELAYS];
+WhiteNoise NOISE[N_DELAYS];
+
+OnePole OUTPUT_DELAY_LPF;
 
 void InitDelays(float samplerate) {
   for (int i = 0 ; i < N_DELAYS ; i++) {
     DELAY_LINES[i].Init();
+    NOISE[i].Init();
+
     DELAYS[i].del = &DELAY_LINES[i];
     DELAYS[i].currentDelay = samplerate * 1.0f;
     DELAYS[i].feedback = 0.9;
     DELAYS[i].lastVal = 0;
+    DELAYS[i].samplerate = samplerate;
+    DELAYS[i].noise = &NOISE[i];
 
     OnePole fb_lpf;
     OnePole fb_hpf;
@@ -97,21 +117,32 @@ void InitDelays(float samplerate) {
     FB_LPFS[i].Init();
     FB_LPFS[i].SetFilterMode(OnePole::FILTER_MODE_LOW_PASS);
     FB_LPFS[i].SetFrequency(8000.0f / samplerate);
-    
-    // HPF filter at 80hz to make sure the resulting audio mash doesnt
+
+    // HPF filter at 60hz to make sure the resulting audio mash doesnt
     // get super muddy in the sub frequencies.
     FB_HPFS[i].Init();
     FB_HPFS[i].SetFilterMode(OnePole::FILTER_MODE_HIGH_PASS);
-    FB_HPFS[i].SetFrequency(80.0f / samplerate);
-    
+    FB_HPFS[i].SetFrequency(60.0f / samplerate);
+
+    TAPE_HPFS[i].Init();
+    TAPE_HPFS[i].SetFilterMode(OnePole::FILTER_MODE_HIGH_PASS);
+    TAPE_HPFS[i].SetFrequency(5000.0f / samplerate);
+
+    DELAYS[i].tape_hpf = &TAPE_HPFS[i];
     DELAYS[i].fb_lpf = &FB_LPFS[i];
     DELAYS[i].fb_hpf = &FB_HPFS[i];
 
     // differing LFOs for each delay line
     LFOS[i].Init(samplerate);
-    LFOS[i].SetFreq(0.05f + (0.03f * i));
-    LFOS[i].SetAmp(1.2f + (0.2 * i));
+    LFOS[i].SetFreq(0.005f + (0.003f * i));
+
+    // lfo is 100ms max (plus some variation from the samplerate,
+    // we'll scale these for each delay time in the delay loop
+    LFOS[i].SetAmp((0.1 + (0.05 * i)) * samplerate);
     DELAYS[i].lfo = &LFOS[i];
+
+    // set the noise amp
+    NOISE[i].SetAmp(0.00005f);
   }
 }
 
@@ -126,13 +157,14 @@ void AudioCallback(AudioHandle::InputBuffer in,
 
   RECORDING = hw.switches[Hothouse::FOOTSWITCH_1].Pressed();
   ERASING   = hw.switches[Hothouse::FOOTSWITCH_2].Pressed();
-
   INPUT_SCALE = RECORDING ? 1.0f : 0.0f;
 
   // feedback and crossfeedback are ez-here.
   CROSS_FEEDBACK  = hw.knobs[0].Process();
   DELAY_TIME_KNOB = hw.knobs[1].Process();
-  FEEDBACK        = hw.knobs[2].Process();
+  // allow individual feedback to have some slight amplification for
+  // feedback madness.
+  FEEDBACK        = hw.knobs[2].Process() * 1.1f;
 
   // fade in the input.
   fonepole(CURRENT_INPUT_SCALE, INPUT_SCALE, 0.02f);
@@ -145,14 +177,14 @@ void AudioCallback(AudioHandle::InputBuffer in,
   }
 
   DELAYS[0].feedback = DELAYS[1].feedback = FEEDBACK;
-  
+
   for (size_t i = 0; i < size; ++i) {
     // update delays
     float mix = 0;
 
     // input scale is used here to fade in input to the delay lines
     float in_samp = in[0][i] * CURRENT_INPUT_SCALE;
-    
+
     // manually unrolled the mixing loop
     float sig = DELAYS[0].Process(in_samp + (CROSS_FEEDBACK * DELAYS[1].lastVal));
     mix += sig;
@@ -176,7 +208,7 @@ int main() {
   hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
 
   InitDelays(hw.AudioSampleRate());
-  
+
   led_record.Init(hw.seed.GetPin(Hothouse::LED_1), false);
   led_erase.Init(hw.seed.GetPin(Hothouse::LED_2), false);
 
@@ -196,18 +228,18 @@ int main() {
     // the audio loop
     if (DELAY_TIME_KNOB < 0.25f) {
       DELAYS[0].currentDelay = 2.503f * hw.AudioSampleRate();
-      DELAYS[1].currentDelay = 1.721f * hw.AudioSampleRate();
+      DELAYS[1].currentDelay = 2.221 * hw.AudioSampleRate();
     } else if (DELAY_TIME_KNOB < 0.50f) {
-      DELAYS[0].currentDelay = 5.0f * hw.AudioSampleRate();
-      DELAYS[1].currentDelay = 3.0f * hw.AudioSampleRate();
+      DELAYS[0].currentDelay = 9.203f * hw.AudioSampleRate();
+      DELAYS[1].currentDelay = 5.0f * hw.AudioSampleRate();
     } else if (DELAY_TIME_KNOB < 0.75f) {
       DELAYS[0].currentDelay = 17.0f * hw.AudioSampleRate();
-      DELAYS[1].currentDelay = 11.0f * hw.AudioSampleRate();      
+      DELAYS[1].currentDelay = 11.0f * hw.AudioSampleRate();
     } else {
       DELAYS[0].currentDelay = 23.0f * hw.AudioSampleRate();
-      DELAYS[1].currentDelay = 17.0f * hw.AudioSampleRate();      
+      DELAYS[1].currentDelay = 17.0f * hw.AudioSampleRate();
     }
   }
-  
+
   return 0;
 }
